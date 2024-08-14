b2a4e4b9cbd6bf685d1102a63bce335ac395c96c
Dhruvin Patel <dhruvinvpatel12@gmail.com>
2024-08-14 17:04:49 -0400

    Added new functions and class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 using namespace std;
+
+
 
 // structure for physical description of criminal 
 struct Description{
@@ -34,7 +37,7 @@ struct Date{
 
 
 // class for Criminal Record
-class Criminal_record{
+class Criminal{
     private:
         string name;
         string sex;
@@ -47,10 +50,10 @@ class Criminal_record{
         Date date;
 
     public:
-        Criminal_record() {}// constructor
+        Criminal() {}// constructor
 
         // function to input details of criminal record
-        void addRecord(){
+        void input(){
             cout<<"Enter convict number : ";
             getline(cin,convict_no);
 
@@ -63,7 +66,7 @@ class Criminal_record{
             cout<<"Enter age: ";
             cin>>age;
 
-            cout<<"Enter Description of Criminal";
+            cout<<"Enter Description of Criminal\n";
             cout<<"Color: ";
             getline(cin,description.color);
             cout<<"Face: ";
@@ -73,6 +76,105 @@ class Criminal_record{
             cout<<"Identification mark: ";
             getline(cin,description.identification_mark);
 
+            cout<<"Enter court details\n";
+            cout<<"Court: ";
+            getline(cin,court.court_name);
+            cout<<"Address: ";
+            getline(cin,court.court_address);
+            cout<<"Crime: ";
+            getline(cin,court.crime);
+            cout<<"Crime Act:";
+            getline(cin,court.act);
+
+            cout<<"Enter Police deatials\n";
+            cout<<"Enter FIR number: ";
+            getline(cin,police.fir_No);
+            cout<<"Enter police station address: ";
+            getline(cin,police.police_station_address);
+
+            cout<<"Enter Date: (dd mm yyyy) ";
+            cin>>date.day>>date.month>>date.year;
+        }
+
+        // getter for convict number 
+        string getConvictNo() const{
+            return convict_no;
+        }
+
+};
+
+class Criminal_record{
+    private:
+        vector<Criminal> rec;;
+
+     public:
+        Criminal_record() {}
+
+        void addRecord(){
+             Criminal newCriminal;
+             newCriminal.input();
+             rec.push_back(newCriminal);
+             cout<<"\nRecord Added\n";
+        }
+
+        void modifyRecord(){
+            string convict_no;
+            cout<<"Enter convict number to modify record: ";
+            getline(cin,convict_no);
+
+            bool found = false;
+            for(int i =1; i<rec.size(); i++){
+                if (rec[i].getConvictNo() == convict_no){
+                    rec[i].input();
+                    found = true;
+                    break;
+                }
+            }
+            if(found){
+                cout<<"Record updated\n";
+            }
+            else{
+                cout<<"Record not found\n";
+            }
+        }
+
+        void deleteRecord(){
+            string convict_no;
+            cout<<"Enter convict number to delete record: ";
+            getline(cin,convict_no);
+
+            bool found = false;
+            for(int i =1; i<rec.size(); i++){
+                if (rec[i].getConvictNo() == convict_no){
+                    rec.erase(rec.begin()+i);
+                    found = true;
+                    break;
+                }
+            }
+            if(found){
+                cout<<"Record updated\n";
+            }
+            else{
+                cout<<"Record not found\n";
+            }
+        }
+
+        void displayRecord(){
+            string convict_no;
+            cout<<"Enter convict number to display: ";
+            getline(cin,convict_no);
+
+            bool found=false;
+            for(int i = 1; i<rec.size();i++){
+                if(rec[i].getConvictNo() == convict_no){
+                    rec[i].display();
+                    found =true;
+                    break;
+                }
+            }
+            if(!found){
+                cout<<"Record not found\n";
+            }
         }
 
 };
@@ -87,8 +189,9 @@ int main()
         cout<<"\n CRIMINAL RECORD SYSTEM \n";
         cout<<" 1. Enter a new record \n";
         cout<<" 2. Modify a existing record \n";
-        cout<<" 3. Delete a record \n ";
-        cout<<" 4. Exit \n\n";
+        cout<<" 3. Delete a record \n";
+        cout<<" 4. Display record of a crimina\n";
+        cout<<" 5. Exit \n\n";
         cout<<" Enter your choice : ";
         cin>>choice;
 
@@ -100,12 +203,14 @@ int main()
                     break;
             case 3: record.deleteRecord();
                     break;
-            case 4: cout<<" Exiting... \n";
+            case 4: record.displayRecord();
+                    break;
+            case 5: cout<<" Exiting... \n";
                     break;
             default: cout<<"\t Invalid choice. Please enter again. \n";
            
         }
-    }while(choice !=4);
+    }while(choice !=5);
 
     return 0;
 }
